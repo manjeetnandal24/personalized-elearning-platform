abe1b3be92264c8d698ccfdaf0c97a023727156c
Manjeet <manjeetnandalofficial@gmail.com>
2026-06-07 13:13:49 +0530

    Initial React TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <h1>Personalised E-Learning Platform</h1>
+      <p>Learn smarter and track your progress.</p>
+
+      <button>Explore Courses</button>
+    </main>
+  );
+}
+
+export default App;
